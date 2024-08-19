31896aa0b3fef1725b6bccdc022fc56d31b9617a
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2024-08-19 23:14:48 +0530

    Time: 2 ms (19.05%), Space: 45.6 MB (28.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,28 +1,25 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-         int rowLength = matrix.length;
-         int columnLength = matrix[0].length;
-         HashSet<Integer> set1 = new HashSet<>();
-         HashSet<Integer> set2 = new HashSet<>();
-
-         for(int row=0;row<rowLength;row++){
-            for(int column =0;column<columnLength;column++){
-                if(matrix[row][column]==0){
-                    set1.add(row);
-                    set2.add(column);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    set1.add(i);
+                    set2.add(j);
                 }
             }
-         }
-         for(int row =0;row<rowLength;row++){
-            for(int column =0;column<columnLength;column++){
-                if(set1.contains(row)|| set2.contains(column)){
-                     matrix[row][column]=0;
-                }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+              if(set1.contains(i) || set2.contains(j)){
+                matrix[i][j] =0;
+              }
+                
             }
-         }
-
-
-  }
-         }
-         
-    
+        }
+       
+    }
+}
