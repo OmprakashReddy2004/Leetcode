8a6ebf183b77775ac76a977ed5a78816555f4d24
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2024-09-11 08:30:02 +0530

    Time: 0 ms (100%), Space: 45.7 MB (86.77%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,39 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int result [] = new int[]{-1,-1};
-        int left = 0;
+        int res[] = new int[]{-1,-1};
+        int left =0;
         int right = nums.length-1;
-        int result1 = -1;
-        int result2 = -1;
+        int first = -1;
+        int last = -1;
+        
         while(left<=right){
-            int mid = left +(right-left)/2;
+            int mid = left + (right-left)/2;
             if(nums[mid]==target){
-                result1=mid;
-                right=mid-1;
-            } else if(nums[mid]<target){
-                left = mid+1;
-            } else{
-                right = mid-1;
+                first= mid;
+                right = mid -1;
+            }else if(nums[mid]<target){
+                left = mid + 1;
+            }else{
+                  right = mid-1;
             }
+              
+             
         }
         left =0;
         right = nums.length-1;
-         while(left<=right){
-            int mid = left +(right-left)/2;
+        while(left<=right){
+            int mid = left + (right-left)/2;
             if(nums[mid]==target){
-                result2=mid;
-                left = mid+1;
-            } else if(nums[mid]<target){
-                left = mid+1;
-            } else{
-                right = mid-1;
+                last = mid;
+                left = mid + 1;
+            }else if (nums[mid]<target){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
             }
         }
-        result[0] = result1;
-        result[1] = result2;
-        return result;
-    
-            }
+        res[0] = first;
+        res[1] = last;
+        return res;
+    }
 }
