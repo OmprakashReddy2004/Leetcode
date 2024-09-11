bcdebac39a930b6172d7582658030be1e611db2b
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2024-09-11 20:07:58 +0530

    Time: 0 ms (100%), Space: 42.9 MB (85.07%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -7,25 +7,25 @@ class Solution {
             if(nums[mid]==target){
                 return true;
             }
-            if(nums[left]==nums[right] && nums[mid]==nums[right]){
-                left++;
-                right--;
-                continue;
-            }
-            if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && target<=nums[mid]){
-                    right = mid - 1;
-                } else {
-                    left = mid+1;
-                }
-            } else{
-                if(nums[mid]<=target&& target<=nums[right]){
+            if(nums[left] == nums[mid] && nums[mid]==nums[right]){
+                left ++;
+                right --;
+            }else if(nums[mid]>=nums[left]){
+                if(target>=nums[left] && target<nums[mid]){
+                   right = mid-1;
+                 }else{
                     left = mid + 1;
-                } else{
-                    right = mid-1;
+                 }
+            }else{
+                if(nums[mid]<target && target<=nums[right]){
+                     left = mid + 1;
+                }else{
+                    right = mid -1;
                 }
-            }
+               
         }
-        return false;
+       
     }
+     return false;
+}
 }
