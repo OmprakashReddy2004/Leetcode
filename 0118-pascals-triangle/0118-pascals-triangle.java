35933d3e30afade782ee28e5f2b00c9e06656ee1
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2025-02-21 22:29:09 +0530

    Time: 0 ms (100%), Space: 42.5 MB (8.24%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,29 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        int n = numRows;
-        int [][] matrix = new int [n][n];
-         List<List<Integer>> list = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==0){
-                    matrix[i][j] = 1;
-                }
-            }
+        List<List<Integer>> Resultlist = new ArrayList<>();
+        for(int i=1;i<=numRows;i++){
+            Resultlist.add(helper(i));
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<n;j++){
-                matrix[i][j] = matrix[i-1][j] + matrix[i-1][j-1];
-            }
-        }
-        for(int i=0;i<n;i++){
-            List<Integer> row = new ArrayList<>();
-            for(int j=0;j<n;j++){
-               if(matrix[i][j]!=0){
-                  row.add(matrix[i][j]);
-               }
-            }
-            list.add(row);
-        }
-          return list;
+        return Resultlist;
     }
+    public List<Integer> helper(int row){
+      int ans = 1;
+       List<Integer> list = new ArrayList<>();
+       list.add(ans);
+       for(int col = 1;col<row;col++){
+           ans = ans * (row-col);
+           ans = ans/col;
+           list.add(ans);
+       }
+       return list;
+    } 
 }
