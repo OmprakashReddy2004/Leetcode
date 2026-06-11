bbeec35288f1036a4093a4d734cd06ed4cf51e54
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-06-11 15:58:58 +0530

    Time: 0 ms (100%), Space: 48.1 MB (49.67%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,38 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int res[] = new int[]{-1,-1};
-        int left =0;
-        int right = nums.length-1;
-        int first = -1;
-        int last = -1;
-        
+        int n = nums.length;
+        int left = 0;
+        int right = n-1;
+        int start = -1;
+        int end = -1;
+
         while(left<=right){
             int mid = left + (right-left)/2;
+
             if(nums[mid]==target){
-                first= mid;
-                right = mid -1;
-            }else if(nums[mid]<target){
-                left = mid + 1;
-            }else{
-                  right = mid-1;
-            }
-              
-             
-        }
-        left =0;
-        right = nums.length-1;
-        while(left<=right){
-            int mid = left + (right-left)/2;
-            if(nums[mid]==target){
-                last = mid;
-                left = mid + 1;
-            }else if (nums[mid]<target){
-                left = mid + 1;
-            }else{
+                 start = mid;
+                 right = mid - 1;
+            }else if(nums[mid]>target){
                 right = mid - 1;
+            }else{
+                left = mid + 1;
             }
         }
-        res[0] = first;
-        res[1] = last;
-        return res;
+        left = 0;
+        right = n-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+
+            if(nums[mid]==target){
+                 end = mid;
+                 left = mid + 1;
+            }else if(nums[mid]>target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return new int[]{start,end};
     }
 }
