9a1dc4e20c362ed7f2c27b9fa0449a7a2a563125
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-06-17 14:05:35 +0530

    Time: 0 ms (100%), Space: 44.3 MB (8.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -3,14 +3,21 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
-            }
-        }
+       int lo = 0;
+       int hi = (n * m) - 1;
 
-        return false;
+       while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+
+            if(matrix[mid/m][mid%m]== target){
+                return true;
+            }else if(matrix[mid/m][mid%m] < target){
+                lo = mid + 1;
+            }else{
+                hi = mid - 1;
+            }
+
+       }
+       return false;
     }
 }
