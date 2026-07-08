0cf9dfdc09a11d78cd56ae8c314364400e73c712
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-07-08 12:52:30 +0530

    Time: 5 ms (99.59%), Space: 45.4 MB (29.26%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,28 +1,35 @@
 class Solution {
-    private void findCombinations(int index,int []arr,int target,List<List<Integer>>ans,List<Integer>list){
- 
-            if(target==0){
-                ans.add(new ArrayList<>(list));
-                  return;
-            }
-          
-       
-        for(int i = index;i<arr.length;i++){
-            if(i>index && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            list.add(arr[i]);
-             findCombinations(i+1,arr,target-arr[i],ans,list);
-             list.remove(list.size()-1);
-        
-        }
-        
-      
-
-    }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        findCombinations(0,candidates,target,ans,new ArrayList<>());
-        return ans;
+       List<List<Integer>> list = new ArrayList<>();
+       findCombinations(0,target,candidates,list,new ArrayList<>());
+       return list;
+    }
+
+    public static void findCombinations(int idx, int target, int [] arr, List<List<Integer>> list, List<Integer> ds){
+
+        
+            if(target == 0){
+                list.add(new ArrayList<>(ds));
+                return;
+            }
+            for(int i = idx;i<arr.length;i++){
+                
+                if(i > idx && arr[i]==arr[i-1]){
+                    continue;
+                }
+
+                if(arr[i] > target){
+                    break;
+                }
+                ds.add(arr[i]);
+                findCombinations(i+1, target - arr[i], arr,list, ds);
+                ds.remove(ds.size()-1);
+            }
+        
+             
+             
+
+         
     }
 }
