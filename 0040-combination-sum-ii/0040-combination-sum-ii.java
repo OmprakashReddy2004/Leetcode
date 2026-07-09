d0ea399145a490f9e614a96dcd81855c9be71471
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-07-09 15:12:51 +0530

    Time: 7 ms (38.22%), Space: 45.3 MB (53.3%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,35 +1,36 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-       List<List<Integer>> list = new ArrayList<>();
-       findCombinations(0,target,candidates,list,new ArrayList<>());
-       return list;
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(0,target,candidates,result,new ArrayList<>());
+        return result;
     }
+    public static void dfs(int idx,int target, int []arr, List<List<Integer>> result,List<Integer> ds){
 
-    public static void findCombinations(int idx, int target, int [] arr, List<List<Integer>> list, List<Integer> ds){
+        if(target==0){
+            result.add(new ArrayList<>(ds));
+            return;
+        }
 
-        
-            if(target == 0){
-                list.add(new ArrayList<>(ds));
-                return;
+        if(target < 0){
+            return;
+        }
+        int prev = -1;
+        for(int i=idx;i<arr.length;i++){
+
+            if(arr[i] == prev){
+                continue;
             }
-            for(int i = idx;i<arr.length;i++){
-                
-                if(i > idx && arr[i]==arr[i-1]){
-                    continue;
-                }
+            //include 
+            ds.add(arr[i]);
+            dfs(i+1,target-arr[i],arr,result,ds);
 
-                if(arr[i] > target){
-                    break;
-                }
-                ds.add(arr[i]);
-                findCombinations(i+1, target - arr[i], arr,list, ds);
-                ds.remove(ds.size()-1);
-            }
+            //not include
+            ds.remove(ds.size() - 1);
+            prev = arr[i];
+           
+        }
+          
         
-             
-             
-
-         
     }
 }
