4206e90a7a3f64c25136b1374b312865a23ffa48
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-07-09 16:18:11 +0530

    Time: 20 ms (32.1%), Space: 98 MB (65.56%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,28 +1,24 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> res = new ArrayList<>();
-        int [] arr = new int [n+1];
-        for(int i=1;i<=n;i++){
-            arr[i] = i;
-        }
-        dfs(1,k,arr,res,new ArrayList<>());
+        dfs(1,k,n,res,new ArrayList<>());
         return res;
     }
-    public static void dfs(int idx,int k,int [] arr,List<List<Integer>> res, List<Integer> ds){
+    public static void dfs(int idx,int k,int n,List<List<Integer>> res, List<Integer> ds){
 
         if(ds.size()==k){
             res.add(new ArrayList<>(ds));
             return;
         }
 
-        if(idx >= arr.length){
-            return;
+        for(int i=idx;i<=n;i++){
+            ds.add(i);
+            dfs(i+1,k,n,res,ds);
+            ds.remove(ds.size() -1);
         }
 
-        ds.add(arr[idx]);
-        dfs(idx+1,k,arr,res,ds);
-        ds.remove(ds.size() -1);
-        dfs(idx+1,k,arr,res,ds);
+        
+        
 
     }
 }
