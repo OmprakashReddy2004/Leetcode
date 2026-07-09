09b5f58b78d03e76cdf9549cedb4d68a663c407a
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-07-09 15:45:34 +0530

    Time: 2 ms (99.7%), Space: 45.1 MB (42.54%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,19 +1,26 @@
 class Solution {
-    public void func(int index,int[] nums,List<Integer> ds,List<List<Integer>> ansList){
-        ansList.add(new ArrayList<>(ds));
-        for(int i =index;i<nums.length;i++){
-            if(i!=index && nums[i]==nums[i-1]) continue;
-                ds.add(nums[i]);
-                func(i+1,nums,ds,ansList);
-                ds.remove(ds.size()-1);
-
-            
-        }
-    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ansList = new ArrayList<>();
-        func(0,nums,new ArrayList<>(),ansList);
-        return ansList;
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(0, nums, res, new ArrayList<>());
+        return res;
+    }
+
+    public static void dfs(int idx, int[] arr, List<List<Integer>> res, List<Integer> ds) {
+
+        res.add(new ArrayList<>(ds));
+        int prev = -11;
+
+        for (int i = idx; i < arr.length; i++) {
+
+            if (arr[i] == prev) {
+                continue;
+            }
+
+            ds.add(arr[i]);
+            dfs(i + 1, arr, res, ds);
+            ds.remove(ds.size() - 1);
+            prev = arr[i];
+        }
     }
 }
