b3c08f2efbeda35b5bfa346d53449eeab77f2fe9
om prakash reddy <161311199+OmprakashReddy2004@users.noreply.github.com>
2026-07-12 22:01:19 +0530

    Time: 27 ms (98.42%), Space: 77 MB (25.63%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int n = arr.length;
+         int [] nums = new int[n];
+         for(int i=0;i<n;i++){
+            nums[i] = arr[i];
+         }
+         Arrays.sort(nums);
+         HashMap<Integer,Integer> map = new HashMap<>();
+
+       int rank = 1;
+
+        for (int i = 0; i < n; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], rank);
+                rank++;
+            }
+        }
+
+         for(int i=0;i<n;i++){
+            arr[i] = map.get(arr[i]); 
+         }
+         
+    return arr;
+}
+}
